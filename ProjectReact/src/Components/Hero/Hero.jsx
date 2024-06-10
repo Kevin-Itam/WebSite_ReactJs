715ca1adb57fb4ddfa2f1841cbf8e29e253b5fd3
Kevin-Itam <kevinitamar16@gmail.com>
2024-06-10 19:24:24 -0300

    10/06/2024

---
+++ ProjectReact/src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+
+
+const Hero = () => {
+    return (
+        <div className='hero container'>
+            <div className='hero-text'>
+                <h1>We ensure better education for a better world</h1>
+                <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills and experiences needed to escel in the dynamic field of education</p>
+                <button className='btn'>Explore more</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
