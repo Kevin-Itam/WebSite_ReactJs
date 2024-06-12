d21707cba45d7740426f22b81ff41e5dfec7e10b
Kevin-Itam <kevinitamar16@gmail.com>
2024-06-11 21:10:03 -0300

    11/06/2024 complete

--- ProjectReact/src/App.jsx
+++ ProjectReact/src/App.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
-
+import Programs from './Components/programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+import Videoplayer from './Components/VideoPlayer/Videoplayer'
+import { useState } from 'react' 
 const App = () => {
+
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle='Our Program' title='What we Offort'/>
+        <Programs />
+        <About setPlayState={setPlayState}/>
+        <Title subTitle='Gallery' title='Campus photos'/>
+        <Campus/>
+        <Title subTitle='TESTIMONIALS' title='What student says'/>
+        <Testimonials/>
+        <Title subTitle='Contact Us' title='Get in Touch'/>
+        <Contact/>
+        <Footer/>
+      </div>
+      <Videoplayer playState={playState} setPlayState={setPlayState}/>
     </div>
   )
 }
---
+++ ProjectReact/src/Components/About/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = ({setPlayState}) => {
+    return (
+        <div className='about'>
+            <div className="about-left">
+                <img src={about_img} alt="" className='about-img' />
+                <img src={play_icon} alt="" className='play-icon'  onClick={()=>{setPlayState(true)}}/>
+            </div>
+            <div className="about-right">
+                <h3>About University</h3>
+                <h2>Embark on a transformative educationnal journey with our</h2>
+                <p>What is Lorem Ipsum?
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
